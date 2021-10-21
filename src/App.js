5e7a19285f16181749bc7db708c84fbdbda08a19
Avni Rastogi <avesrastogi@gmail.com>
2021-10-21 19:56:54 +0530

    modified for getCount

--- src/App.js
+++ src/App.js
@@ -70,9 +70,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <section 
-          value={item.quantity}
-          onChange={(e) => changeQuantity(e.target.value)}>
+        <section>
             Cart Items - {getCount}
           </section>          
         <section>Total Cart Price - {getTotalPrice}</section> 
